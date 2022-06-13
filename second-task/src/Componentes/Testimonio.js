532cf2b481e99c64b91d4704542adaeac68387e1
Fabian Portilla <flportilla@gmail.com>
2022-06-13 17:15:41 -0500

    Add first component and img

---
+++ second-task/src/Componentes/Testimonio.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+function Testimonio() {
+  return (
+    <div className='witness_container'>
+      <img
+        className='witness_img'
+        alt="witness img"
+        src='../img/witness1.png'
+      />
+    </div>
+  )
+}
+
+export default Testimonio
